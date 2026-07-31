03266c8b4a84265ea3a1d158ed2a84ebe8de7e4a
root <root@DESKTOP-IVHFQKO.localdomain>
2026-07-31 10:30:56 -0600

    first commit

---
+++ app/Console/Commands/ConsultarArchivosEntrada.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Tramite;
+use Exception;
+use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+
+#[Signature('consultar_entrada')]
+#[Description('Comando para obtener los archivos de entrada (servicios)')]
+class ConsultarArchivosEntrada extends Command
+{
+
+
+    public function handle()
+    {
+
+        $url = 'http://10.0.32.14/finanzas/solicitud/';
+
+        try {
+
+            $response =  Http::get($url);
+
+            if($response->status() !== 200){
+
+                throw new Exception("Error de comunicación con la url: " . $url);
+
+            }
+
+            $html = $response->body();
+
+            preg_match_all('/href="([^"]+)"/i', $html, $matches);
+
+            $archivos = collect($matches[1])
+                        ->filter(function ($archivo) {
+                            return str_contains($archivo, now()->format('Ym'));
+                        })
+                        ->values();
+
+            foreach($archivos as $archivo){
+
+                $entrada = Tramite::where('tramite', $archivo)->first();
+
+                if(! $entrada){
+
+                    $contenido = Http::get($url.$archivo)->body();
+
+                    Storage::disk('s3')->put('cobol/entrada/'. $archivo, $contenido);
+
+                    Tramite::create(['tramite' => $archivo]);
+
+                }
+
+            }
+
+        } catch (Exception $ex) {
+
+            Log::error('Error al consultar archivos de entrada (servicios) ' . $ex->getMessage());
+
+        } catch (\Throwable $th) {
+
+            Log::error('Error al consultar archivos de entrada (servicios) ' . $th);
+
+        }
+
+    }
+}
---
+++ app/Console/Commands/InsertarArchivosSalida.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Salida;
+use App\Models\Tramite;
+use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+
+#[Signature('insertar_salida')]
+#[Description('Comando para insertar los archivos de los servicios pagados')]
+class InsertarArchivosSalida extends Command
+{
+
+    public function handle()
+    {
+
+        try {
+
+            $archivos = Tramite::where('procesado', 0)->get();
+
+            $todosLosArchivos = Storage::disk('s3')->files('cobol/salida/');
+
+            foreach ($archivos as $archivo) {
+
+                $nombre = str_replace('.TXT', '', $archivo->tramite);
+
+                foreach ($todosLosArchivos as $archivo_s3) {
+
+                    if(str_contains($archivo_s3, $nombre)){
+
+                        $contenido = Storage::disk('s3')->get($archivo_s3);
+
+                        Salida::create([
+                            'archivo' => $nombre,
+                            'contenido' => $contenido
+                        ]);
+
+                        $archivo->update(['procesado' => true]);
+
+                    }
+
+                }
+
+            }
+
+        } catch (\Throwable $th) {
+
+            Log::error('Error al insertar archivos de salida (servicios) ' . $th);
+
+        }
+
+    }
+
+}
